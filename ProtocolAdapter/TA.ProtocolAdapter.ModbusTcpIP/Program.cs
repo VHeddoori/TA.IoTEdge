d0c2edddc5bfdfa1b92c1f3902c1f01220a32c82
Vishwanath Heddoori <vishwanath.heddoori@gmail.com>
2021-04-03 06:40:51 +0530

    Project structure

---
+++ ProtocolAdapter/TA.ProtocolAdapter.ModbusTcpIP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TA.ProtocolAdapter.ModbusTcpIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TA.IoTEdgeApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TA.IoTEdgeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
